e6a68ff5cdc2b03ac7e82dea896c3a53e70e95fb
Oliver Sun <olisun@mac.com>
2020-05-20 19:11:54 -0700

    set-up server with typeDefs and resolvers for dummy data links and feed

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,51 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+let links = [
+  {
+    id: '1',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  },
+  {
+    id: '2',
+    url: 'www.espn.com',
+    description: 'Sports website'
+  },
+  {
+    id: '3',
+    url: 'www.dev.to',
+    description: 'Developers blog'
+  }
+]
+
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+    link(id: ID!): Link!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
-    info: () => `Testing the API.`
+    info: () => {
+      return `Testing the API.`
+    },
+    feed: () => {
+      return links
+    },
+    link: (args, { id }) => {
+      const link = links.find(args => args.id === id);
+      return link
+    }
   }
-};
+}
 
 const server = new GraphQLServer({
   typeDefs,
