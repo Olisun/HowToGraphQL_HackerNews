8a4e3370b0a09e4ad9bd8a6856ca165b8b06d9e8
Oliver Sun <olisun@mac.com>
2020-05-27 13:15:05 -0700

    added login and signup types and mutations to schema. refactored by breaking up resolvers into sep. files

---
+++ server/src/resolvers/Mutation.js
@@ -0,0 +1,33 @@
+async function signup(parent, args, context, info) {
+  const hashedPassword = await bcrypt.hash(args.password, 10)
+  const { password, ...user } = await context.prisma.createUser({ ...args, password: hashedPassword })
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user
+  }
+}
+
+async function login(parent, args, context, info) {
+  const { password, ...user } = await context.prisma.user({ email: args.email })
+  if (!user) {
+    throw new Error('No user found.  Please signup.')
+  }
+
+  const valid = await bcrypt.compare(args.password, password)
+  if (!valid) {
+    throw new Error('Invalid password')
+  }
+
+  return {
+    token,
+    user
+  }
+}
+
+module.exports = {
+  signup,
+  login,
+  post
+}
