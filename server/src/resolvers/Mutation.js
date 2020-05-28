828513f574cbad52f9824a559e3b44048fc61c6b
Oliver Sun <olisun@mac.com>
2020-05-27 20:21:57 -0700

    added signup and login logic to resolver functions. added getUserId util function to pass the JWT along with the user.

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,7 +1,13 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET, getUserId } = require('../utils');
+
 async function signup(parent, args, context, info) {
-  const hashedPassword = await bcrypt.hash(args.password, 10)
-  const { password, ...user } = await context.prisma.createUser({ ...args, password: hashedPassword })
-  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  const hashedPassword = await bcrypt.hash(args.password, 10);
+  const { password, ...user } = await context.prisma.createUser({
+    ...args, password: hashedPassword
+  });
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
   return {
     token,
@@ -10,20 +16,33 @@ async function signup(parent, args, context, info) {
 }
 
 async function login(parent, args, context, info) {
-  const { password, ...user } = await context.prisma.user({ email: args.email })
+  const { password, ...user } = await context.prisma.user({ email: args.email });
   if (!user) {
-    throw new Error('No user found.  Please signup.')
+    throw new Error('No user found.  Please signup.');
   }
 
-  const valid = await bcrypt.compare(args.password, password)
+  const valid = await bcrypt.compare(args.password, password);
   if (!valid) {
-    throw new Error('Invalid password')
+    throw new Error('Invalid password');
   }
 
   return {
     token,
     user
   }
+}
+
+function post(parent, args, context, info) {
+  const userId = getUserId(context);
+  return context.prisma.createLink({
+    url: args.url,
+    description: args.description,
+    postedBy: {
+      connect: {
+        id: userId
+      }
+    },
+  })
 }
 
 module.exports = {
