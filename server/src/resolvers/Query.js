3747c0e895e08ed826337129d873ec6057a37d79
Oliver Sun <olisun@mac.com>
2020-05-31 13:13:05 -0700

    added a filter to the query schema and resolver

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,5 +1,16 @@
-function feed(parent, args, context, info) {
-  return context.prisma.links();
+async function feed(parent, args, context, info) {
+  // Basically says if no filters are provided, the where object will just be an empty object and no filters will be applied to the query. 
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter }
+    ]
+  } : {};
+
+  const links = await context.prisma.links({
+    where
+  });
+  return links
 }
 
 module.exports = {
