509bdb0987be75d69ffe18e3ac51b62c2561188d
Oliver Sun <olisun@mac.com>
2020-06-02 20:44:05 -0700

    installed apollo-boost react-apollo and graphql. removed boilerplate css and set-up starting point for the front-end

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hackernews Clone</h1>
+    </div>
+  );
+}
+
+export default App;
