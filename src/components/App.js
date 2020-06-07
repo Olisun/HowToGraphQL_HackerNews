faba0a2bb45c780807b8e5fee30a1d1996957a60
Oliver Sun <olisun@mac.com>
2020-06-06 19:39:57 -0700

    added routes for new links and submit a new link pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
+import Header from './Header';
 
 import '../styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <CreateLink />
-      <LinkList />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
     </div>
   );
 }
